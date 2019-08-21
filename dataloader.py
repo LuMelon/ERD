c1561781f49a5e772419e9967037159236513f40
LuMelon <lumenglong2017@gmail.com>
2019-08-21 20:54:05 +0800

    back up

--- dataloader.py
+++ dataloader.py
@@ -1,106 +1,114 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 import re
 import pandas as pd
 import os
 import numpy as np
-# In[ ]:
+import keras
 
-def transIrregularWord(word):
-    if not word:
-        return ''
-    pattern1 = "[^A-Za-z]*$" #punctuation at the end of sentence
-    pattern2 = "^[^A-Za-z@#]*" #punctuation at the start of sentence
-    word = re.sub(pattern2, "", re.sub(pattern1, "", word))
-    pattern3 = '(.*)http(.?)://(.*)' # url
-    pattern4 = '^[0-9]+.?[0-9]+$' # number
-    if not word:
-        return ''
-    elif word.__contains__('@'):
-        return 'person'
-    elif word.__contains__('#'):
-        return 'topic'
-    elif re.match(r'(.*)http?://(.*)', word, re.M|re.I|re.S):    
-        return 'links'
-    elif re.match(pattern4, word, re.M|re.I):
-        return 'number'
-    else:
-        return  word.lower()
+class SentiDataLoader:
+    def __init__(self, dirpath, trainfile, testfile, char_list):
+        self.dirpath = dirpath
+        self.trainfile = trainfile
+        self.testfile = testfile
+        self.s = char_list
+
+    def load_data(self): # charVocab is a char-to-idx dictionary:{char:idx}
+        def transIrregularWord(word):
+            if not word:
+                return ''
+            pattern1 = "[^A-Za-z]*$" #punctuation at the end of sentence
+            pattern2 = "^[^A-Za-z@#]*" #punctuation at the start of sentence
+            word = re.sub(pattern2, "", re.sub(pattern1, "", word))
+            pattern3 = '(.*)http(.?)://(.*)' # url
+            pattern4 = '^[0-9]+.?[0-9]+$' # number
+            if not word:
+                return ''
+        #     elif word.__contains__('@'):
+        #         return 'person'
+            elif word.__contains__('#'):
+                return 'topic'
+            elif re.match(r'(.*)http?://(.*)', word, re.M|re.I|re.S):    
+                return 'links'
+#             elif re.match(pattern4, word, re.M|re.I):
+#                 print("word:", word)
+#                 return 'number'
+            else:
+                return  word.lower()
+
+        def sentence2words(line):
+            words = re.split('([,\n ]+)', line.strip() )
+            words = list( filter(lambda s: len(s)>0, [transIrregularWord(word) for word in words]) )
+            return words
+
+        def word2chars(word, charVocab):
+            rst = [charVocab.get(char) for char in word]
+            for i in range(len(rst)):
+                if rst[i] is None:
+                    print("Unknown char:", word[i])
+                    print("word:", word)
+                    rst[i] = 0
+            return rst
+
+        def CSVFile2Dataset(filepath):
+            print(filepath)
+            df = pd.read_csv(filepath,encoding='latin-1')
+            instances = [(line[-1], line[0]) for line in df.values]
+            del df
+            texts = [sentence2words(instance[0]) for instance in instances]
+            labels = [instance[1] for instance in instances]
+            return texts, labels
+        
+        charVocab = {c:idx for (idx, c) in enumerate(self.s)}
+        texts, self.train_label = CSVFile2Dataset(os.path.join(self.dirpath, self.trainfile))
+        self.train_data = [list(word2chars(word, charVocab) for word in sentence) for sentence in texts]
+        texts, self.test_label = CSVFile2Dataset(os.path.join(self.dirpath, self.testfile))
+        self.test_data = [list(word2chars(word, charVocab) for word in sentence) for sentence in texts]
+        del texts
+        self.max_sent_len = max([max(len(sent) for sent in texts) for texts in [self.train_data, self.test_data]])
+        self.max_char_num = max(
+                                max([max(len(word) for word in sent) for sent in self.test_data]), 
+                                max([max(len(word) for word in sent) for sent in self.train_data])
+                               )
+
+    def GetTrainingBatch(self, batchId, batchsize):
+        def padding_sequence(max_len, sentence):
+            return keras.preprocessing.sequence.pad_sequences(
+                                                        sentence, 
+                                                        maxlen=max_len, 
+                                                        dtype='int32', 
+                                                        padding='post', 
+                                                        truncating='post',
+                                                        value=0.0
+            )
+        
+        startIdx = batchId*batchsize
+        data_y = np.zeros([batchsize, 3], dtype=np.int32)
+        ids = [idx%len(self.train_data) for idx in range(startIdx, startIdx+batchsize, 1)]
+        data_x = np.array(
+                        [padding_sequence(41, self.train_data[x]).tolist() 
+                        for x in ids]
+        )
+        for idx in ids:
+            data_y[idx][int(self.train_label[idx]/2)] = 1
+        return data_x, data_y
     
-def sentence2words(line):
-    words = re.split('([,\n ]+)', line.strip() )
-    words = list( filter(lambda s: len(s)>0, [transIrregularWord(word) for word in words]) )
-    return words
-
-def CSVFile2Dataset(filepath):
-    print(filepath)
-    df = pd.read_csv(filepath,encoding='latin-1')
-    instances = [(line[-1], line[0]) for line in df.values]
-    del df
-    texts = [sentence2words(instance[0]) for instance in instances]
-    labels = [instance[1] for instance in instances]
-    return texts, labels
-
-# dirpath = '/home/hadoop/trainingandtestdata'
-# trainfile = 'training.1600000.processed.noemoticon.csv'
-# testfile = 'testdata.manual.2009.06.14.csv'
-# trainset, trainlabel = CSVFile2Dataset(os.path.join(dirpath, trainfile))
-# testset, testlabel = CSVFile2Dataset(os.path.join(dirpath, testfile))
-trainset = np.load("trainset.npy").tolist()
-testset = np.load("testset.npy").tolist()
-trainlabel = np.load("trainlabel.npy").tolist()
-testlabel = np.load("testlabel.npy").tolist()
-max_sent_len = max(max([len(text) for text in trainset]), max([len(text) for text in testset]))
-
-# import gensim
-# word2vec = gensim.models.KeyedVectors.load_word2vec_format('/home/hadoop/word2vec.model')
-import chars2vec 
-c2vec = chars2vec.load_model('eng_300')
-
-
-def GetTrainingBatch(batchId, batchsize, embedding_dim):
-    data_x = np.zeros([batchsize, max_sent_len, embedding_dim], dtype=np.float32)
-    data_y = np.zeros([batchsize, 3], dtype=np.int32)
-    startIdx = batchId*batchsize
-    # miss_vec = 0
-    # hit_vec = 0
-    trainVol = len(trainlabel)
-    if startIdx >= len(trainset):
-        startIdx = startIdx%len(trainset)
-    for i in range(batchsize):
-        mts = int(np.random.uniform()*trainVol)
-        data_y[i][int(trainlabel[mts]/2)] = 1
-        # for j in range(len(trainset[mts])):
-            # try:
-            #     data_x[i][j] = word2vec[trainset[mts][j]]
-            # except KeyError:
-            #     print("word:", trainset[mts][j])
-            #     miss_vec += 1
-            # except IndexError:
-            #     print("i, j, k:", FLAGS.batch_size, '|',t_data_len[mts] ,'|', len(t_words))
-            #     print("word:", trainset[mts][j], "(", i, j, k, ")")
-            #     raise
-            # else:
-            #     hit_vec += 1
-        data_x[i][:len(trainset[mts])] = c2vec.vectorize_words(trainset[mts])
-    # print("hit_vec | miss_vec:", hit_vec, '|', miss_vec)
-    return data_x, data_y
-
-def GetTestData(batchId, batchsize, embedding_dim):
-    data_x = np.zeros([batchsize, max_sent_len, embedding_dim], dtype=np.float32)
-    data_y = np.zeros([batchsize, 3], dtype=np.int32)
-    startIdx = batchId*batchsize
-    miss_vec = 0
-    hit_vec = 0
-    testVol = len(testlabel)
-    if startIdx >= len(testset):
-        startIdx = startIdx%len(testset)
-    for i in range(batchsize):
-        mts = int(np.random.uniform()*testVol)
-        data_y[i][int(testlabel[mts]/2)] = 1
-        data_x[i][:len(testset[mts])] = c2vec.vectorize_words(testset[mts])
-    # print("hit_vec | miss_vec:", hit_vec, '|', miss_vec)
-    return data_x, data_y
+    def GetTestData(self, batchId, batchsize):
+        def padding_sequence(max_len, sentence):
+            return keras.preprocessing.sequence.pad_sequences(
+                                                        sentence, 
+                                                        maxlen=max_len, 
+                                                        dtype='int32', 
+                                                        padding='post', 
+                                                        truncating='post',
+                                                        value=0.0
+            )
+        
+        startIdx = batchId*batchsize
+        data_y = np.zeros([batchsize, 3], dtype=np.int32)
+        ids = [idx%len(self.test_data) for idx in range(startIdx, startIdx+batchsize, 1)]
+        data_x = np.array(
+                        [padding_sequence(41, self.test_data[x]).tolist() 
+                        for x in ids]
+        )
+        for idx in ids:
+            data_y[idx][int(self.test_label[idx]/2)] = 1
+        return data_x, data_y
