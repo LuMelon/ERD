4ec4ca460134ff5c44ec32504f618ac9889e8d7a
LuMelon <lumenglong2017@gmail.com>
2019-08-29 09:59:40 +0800

    baseline ERD uploaded

---
+++ baslines/ERDProj/ERDMain.py
@@ -0,0 +1,207 @@
+# coding: utf-8
+from collections import deque
+from ERDModel import RL_GRU2
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+from dataUtils import *
+from logger import MyLogger
+os.chdir("/home/hadoop/ERD")
+
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+logger = MyLogger("ERDMain")
+
+def df_train(sess, summary_writer, mm, t_acc, t_steps, new_data_len=[]):
+    sum_loss = 0.0
+    sum_acc = 0.0
+    ret_acc = 0.0
+    init_states = np.zeros([FLAGS.batch_size, FLAGS.hidden_dim], dtype=np.float32)
+
+    for i in range(t_steps):
+        if len(new_data_len) > 0:
+            x, x_len, y = get_df_batch(i, new_data_len)
+        else:
+            x, x_len, y = get_df_batch(i)
+        feed_dic = {mm.input_x: x, mm.x_len: x_len, mm.input_y: y, mm.init_states: init_states, mm.dropout_keep_prob: 0.5}
+        _, step, loss, acc = sess.run([df_train_op, df_global_step, mm.loss, mm.accuracy], feed_dic)
+        summary = tf.Summary(value=[
+                    tf.Summary.Value(tag="df_train_loss", simple_value=loss),
+                    tf.Summary.Value(tag="df_train_accuracy", simple_value=acc),
+                ])
+        summary_writer.add_summary(summary, step)
+
+        sum_loss += loss
+        sum_acc += acc
+
+        if i % 10 == 9:
+            sum_loss = sum_loss / 10
+            sum_acc = sum_acc / 10
+            ret_acc = sum_acc
+            print(get_curtime() + " Step: " + str(step) + " Training loss: " + str(sum_loss) + " accuracy: " + str(sum_acc))
+            logger.info(get_curtime() + " Step: " + str(step) + " Training loss: " + str(sum_loss) + " accuracy: " + str(sum_acc))
+
+            if sum_acc > t_acc:
+                break
+            sum_acc = 0.0
+            sum_loss = 0.0
+
+    print(get_curtime() + " Train df Model End.")
+    logger.info(get_curtime() + " Train df Model End.")
+    return ret_acc
+
+
+def rl_train(sess, mm, t_rw, t_steps):
+    ids = np.array(range(FLAGS.batch_size), dtype=np.int32)
+    seq_states = np.zeros([FLAGS.batch_size], dtype=np.int32)
+    isStop = np.zeros([FLAGS.batch_size], dtype=np.int32)
+    max_id = FLAGS.batch_size
+    init_states = np.zeros([FLAGS.batch_size, FLAGS.hidden_dim], dtype=np.float32)
+    feed_dic = {mm.init_states: init_states}
+    state = sess.run(mm.df_state, feed_dic)
+    D = deque()
+    ssq = []
+    print("in RL the begining")
+    logger.info("in RL the begining")
+    # get_new_len(sess, mm)
+    data_ID = get_data_ID()
+    if len(data_ID) % FLAGS.batch_size == 0: # the total number of events
+        flags = int(len(data_ID) / FLAGS.batch_size)
+    else:
+        flags = int(len(data_ID) / FLAGS.batch_size) + 1
+    for i in range(flags):
+        x, x_len, y = get_df_batch(i)
+        feed_dic = {mm.input_x: x, mm.x_len: x_len, mm.input_y: y, mm.init_states:init_states, mm.dropout_keep_prob: 1.0}
+        t_ssq = sess.run(mm.out_seq, feed_dic)# t_ssq = [batchsize, max_seq, scores]
+        if len(ssq) > 0:
+            ssq = np.append(ssq, t_ssq, axis=0)
+        else:
+            ssq = t_ssq
+
+    print(get_curtime() + " Now Start RL training ...")
+    logger.info(get_curtime() + " Now Start RL training ...")
+    counter = 0
+    sum_rw = 0.0 # sum of rewards
+
+    data_len = get_data_len()
+    while True:
+        if counter > FLAGS.OBSERVE:
+            sum_rw += np.mean(rw)
+            if counter % 200 == 0:
+                sum_rw = sum_rw / 2000
+                print(get_curtime() + " Step: " + str(step) + " REWARD IS " + str(sum_rw))
+                logger.info(get_curtime() + " Step: " + str(step) + " REWARD IS " + str(sum_rw))
+                if sum_rw > t_rw:
+                    print("Retch The Target Reward")
+                    logger.info("Retch The Target Reward")
+                    break
+                if counter > t_steps:
+                    print("Retch The Target Steps")
+                    logger.info("Retch The Target Steps")
+                    break
+                sum_rw = 0.0
+            s_state, s_x, s_isStop, s_rw = get_RL_Train_batch(D)
+            feed_dic = {mm.rl_state: s_state, mm.rl_input: s_x, mm.action: s_isStop, mm.reward:s_rw, mm.dropout_keep_prob: 0.5}
+            _, step = sess.run([rl_train_op, rl_global_step], feed_dic)
+
+        x, y, ids, seq_states, max_id = get_rl_batch(ids, seq_states, isStop, max_id, 0, 3150)
+        batch_dic = {mm.rl_state: state, mm.rl_input: x, mm.dropout_keep_prob: 1.0}
+        isStop, mss, mNewState = sess.run([mm.isStop, mm.stopScore, mm.rl_new_state], batch_dic)
+
+        for j in range(FLAGS.batch_size):
+            if random.random() < FLAGS.random_rate:
+                isStop[j] = np.argmax(np.random.rand(2))
+            if seq_states[j] == data_len[ids[j]]:
+                isStop[j] = 1
+
+        # eval
+        rw = get_reward(isStop, mss, ssq, ids, seq_states)
+
+        for j in range(FLAGS.batch_size):
+            D.append((state[j], x[j], isStop[j], rw[j]))
+            if len(D) > FLAGS.max_memory:
+                D.popleft()
+
+        state = mNewState
+        for j in range(FLAGS.batch_size):
+            if isStop[j] == 1:
+                # init_states = np.zeros([FLAGS.batch_size, FLAGS.hidden_dim], dtype=np.float32)
+                # feed_dic = {mm.init_states: init_states}
+                # state[j] = sess.run(mm.df_state, feed_dic)
+                state[j] = np.zeros([FLAGS.hidden_dim], dtype=np.float32)
+        counter += 1
+
+
+def eval(sess, mm):
+    start_ef = int(eval_flag / FLAGS.batch_size)
+    end_ef = int(len(data_ID) / FLAGS.batch_size) + 1
+    init_states = np.zeros([FLAGS.batch_size, FLAGS.hidden_dim], dtype=np.float32)
+
+    counter = 0
+    sum_acc = 0.0
+
+    for i in range(start_ef, end_ef):
+        x, x_len, y = get_df_batch(i)
+        feed_dic = {mm.input_x: x, mm.x_len: x_len, mm.input_y: y, mm.init_states: init_states, mm.dropout_keep_prob: 1.0}
+        _, step, loss, acc = sess.run([df_train_op, df_global_step, mm.loss, mm.accuracy], feed_dic)
+        counter += 1
+        sum_acc += acc
+
+    print(sum_acc / counter)
+
+if __name__ == "__main__":
+    print(get_curtime() + " Loading data ...")
+    logger.info(get_curtime() + " Loading data ...")
+    # load_data(FLAGS.data_file_path)
+    load_data_fast()
+    print(get_curtime() + " Data loaded.")
+    logger.info(get_curtime() + " Data loaded.")
+    # (self, input_dim, hidden_dim, max_seq_len, max_word_len, class_num, action_num):
+    print(FLAGS.embedding_dim, FLAGS.hidden_dim, FLAGS.max_seq_len, FLAGS.max_sent_len, FLAGS.class_num, FLAGS.action_num)
+    logger.info((FLAGS.embedding_dim, FLAGS.hidden_dim, FLAGS.max_seq_len, FLAGS.max_sent_len, FLAGS.class_num, FLAGS.action_num))
+    mm = RL_GRU2(FLAGS.embedding_dim, FLAGS.hidden_dim, FLAGS.max_seq_len,
+                 FLAGS.max_sent_len, FLAGS.class_num, FLAGS.action_num, FLAGS.sent_num)
+    
+    # df model
+    df_global_step = tf.Variable(0, name="global_step", trainable=False)
+    df_train_op = tf.train.AdagradOptimizer(0.05).minimize(mm.loss, df_global_step)
+
+    # rl model
+    rl_global_step = tf.Variable(0, name="global_step", trainable=False)
+    rl_train_op = tf.train.AdamOptimizer(0.001).minimize(mm.rl_cost, rl_global_step)
+    
+    saver = tf.train.Saver(tf.global_variables(), max_to_keep=4)
+    sess = tf.Session()
+    with sess.as_default():
+        sess.run(tf.global_variables_initializer())
+    summary_writer = tf.summary.FileWriter("./reports/", graph=sess.graph)
+
+    ckpt_dir = "df_saved_erd"
+    checkpoint = tf.train.get_checkpoint_state(ckpt_dir)
+    if checkpoint and checkpoint.model_checkpoint_path:
+        print("--------------Debug1------------------")
+        saver.restore(sess, checkpoint.model_checkpoint_path)
+        print(checkpoint.model_checkpoint_path+" is restored.")
+        logger.info(checkpoint.model_checkpoint_path+" is restored.")
+    else:
+        df_train(sess, summary_writer, mm, 0.80, 20000)
+        saver.save(sess, "df_saved_erd/model")
+        print("df_model "+" saved")
+        logger.info("df_model "+" saved")
+
+    for i in range(20):
+        rl_train(sess, mm, 0.5, 50000)
+        saver.save(sess, "rl_saved_erd/model"+str(i))
+        print("rl_model "+str(i)+" saved")
+        logger.info("rl_model "+str(i)+" saved")
+        new_len = get_new_len(sess, mm)
+        acc = df_train(sess, mm, 0.9, 1000, new_len)
+        saver.save(sess, "df_saved_erd/model"+str(i))
+        print("df_model "+str(i)+" saved")
+        logger.info("df_model "+str(i)+" saved")
+        if acc > 0.9:
+            break
+
+    print("The End of My Program")
+    logger.info("The End of My Program")
---
+++ baslines/ERDProj/dataUtils.py
@@ -0,0 +1,336 @@
+import json
+import os
+import time
+import datetime
+import numpy as np
+import gensim
+from config import *
+import random
+import math
+import chars2vec
+import re
+import tensorflow as tf
+import pickle
+
+files = []
+data = {}
+data_ID = []
+data_len = []
+data_y = []
+# word2vec = gensim.models.KeyedVectors.load_word2vec_format('/home/hadoop/word2vec.model')
+c2vec = chars2vec.load_model('eng_300')
+reward_counter = 0
+eval_flag = 0
+
+def get_data_ID():
+    global data_ID
+    return data_ID
+
+def get_data_len():
+    global data_len
+    return data_len
+
+def get_curtime():
+    return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+
+def list_files(data_path):
+    global data, files
+    fs = os.listdir(data_path)
+    for f1 in fs:
+        tmp_path = os.path.join(data_path, f1)
+        if not os.path.isdir(tmp_path):
+            if tmp_path.split('.')[-1] == 'json':
+                files.append(tmp_path)
+        else:
+            list_files(tmp_path)
+
+
+def str2timestamp(str_time):
+    month = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04',
+             'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08',
+             'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+    ss = str_time.split(' ')
+    m_time = ss[5] + "-" + month[ss[1]] + '-' + ss[2] + ' ' + ss[3]
+    d = datetime.datetime.strptime(m_time, "%Y-%m-%d %H:%M:%S")
+    t = d.timetuple()
+    timeStamp = int(time.mktime(t))
+    return timeStamp
+
+
+def data_process(file_path):
+    ret = {}
+    ss = file_path.split("/")
+    data = json.load(open(file_path, mode="r", encoding="utf-8"))
+    # 'Wed Jan 07 11:14:08 +0000 2015'
+    # print("SS:", ss)
+    ret[ss[6]] = {'label': ss[5], 'text': [data['text'].lower()], 'created_at': [str2timestamp(data['created_at'])]}
+    return ret
+
+def transIrregularWord(word):
+    if not word:
+        return ''
+    pattern1 = "[^A-Za-z]*$" #punctuation at the end of sentence
+    pattern2 = "^[^A-Za-z@#]*" #punctuation at the start of sentence
+    word = re.sub(pattern2, "", re.sub(pattern1, "", word))
+    pattern3 = '(.*)http(.?)://(.*)' # url
+    pattern4 = '^[0-9]+.?[0-9]+$' # number
+    if not word:
+        return ''
+    elif word.__contains__('@'):
+        return 'person'
+    elif word.__contains__('#'):
+        return 'topic'
+    elif re.match(r'(.*)http?://(.*)', word, re.M|re.I|re.S):    
+        return 'links'
+    elif re.match(pattern4, word, re.M|re.I):
+        return 'number'
+    else:
+        return  word
+
+def load_data_fast():
+    global data, data_ID, data_len, data_y, eval_flag
+    with open("data/data_dict.txt", "rb") as handle:
+        data = pickle.load(handle)
+    data_ID = np.load("data/data_ID.npy").tolist()
+    data_len = np.load("data/data_len.npy").tolist()
+    data_y = np.load("data/data_y.npy").tolist()
+    max_sent = max( map(lambda value: max(map(lambda txt_list: len(txt_list), value['text']) ), list(data.values()) ) )
+    print("max_sent:", max_sent, ",  max_seq_len:", max(data_len))
+    tf.flags.DEFINE_integer("max_seq_len", max(data_len), "Max length of sequence (default: 300)")
+    tf.flags.DEFINE_integer("max_sent_len", max_sent, "Max length of sentence (default: 300)")
+    eval_flag = int(len(data_ID) / 4) * 3
+    print("{} data loaded".format(len(data)))    
+
+def sortTempList(temp_list):
+    time = np.array([item[0] for item in temp_list])
+    posts = np.array([item[1] for item in temp_list])
+    idxs = time.argsort().tolist()
+    rst = [[t, p] for (t, p) in zip(time[idxs], posts[idxs])]
+    del time, posts
+    return rst
+
+def load_data(data_path):
+    # get data files path
+    global data, files, data_ID, data_len, eval_flag
+    data = {}
+    files = []
+    data_ID = []
+    data_len = []
+    list_files(data_path) #load all filepath to files
+    max_sent = 0
+    # load data to json
+    for file in files:
+        td = data_process(file) # read out the information from json file, and organized it as {dataID:{'key':val}}
+        for key in td.keys(): # use temporary data to organize the final whole data
+            if key in data:
+                data[key]['text'].append(td[key]['text'][0])
+                data[key]['created_at'].append(td[key]['created_at'][0])
+            else:
+                data[key] = td[key]
+    # convert to my data style
+    for key, value in data.items():
+        temp_list = []
+        for i in range(len(data[key]['text'])):
+            temp_list.append([data[key]['created_at'][i], data[key]['text'][i]])
+        temp_list = sortTempList(temp_list)
+        data[key]['text'] = []
+        data[key]['created_at'] = []
+        ttext = ""
+        last = 0
+        for i in range(len(temp_list)):
+            if temp_list[i][0] - temp_list[0][0] > FLAGS.time_limit * 3600 or len(data[key]['created_at']) >= 100:
+                break
+            if i % FLAGS.post_fn == 0: # merge the fixed number of texts in a time interval
+                if len(ttext) > 0: # if there are data already in ttext, output it as a new instance
+                    words = list( filter(lambda s: len(s)>0, [transIrregularWord(word) for word in re.split('([,\n ]+)', ttext.strip() )]) )
+                    if len(words) > max_sent:
+                        max_sent = len(words)
+                    data[key]['text'].append(words)
+                    data[key]['created_at'].append(temp_list[i][0])
+                ttext = temp_list[i][1]
+            else:
+                ttext += " " + temp_list[i][1]
+            last = i
+        # keep the last one
+        if len(ttext) > 0:
+            words = list( filter(lambda s: len(s)>0, [transIrregularWord(word) for word in re.split('([,\n ]+)', ttext.strip() )]) )
+            if len(words) > max_sent:
+                max_sent = len(words)
+            data[key]['text'].append(words)
+            data[key]['created_at'].append(temp_list[last][0])
+    for key in data.keys():
+        data_ID.append(key)
+    data_ID = random.sample(data_ID, len(data_ID)) #shuffle the data id
+    for i in range(len(data_ID)): #pre processing the extra informations
+        data_len.append(len(data[data_ID[i]]['text']))
+        if data[data_ID[i]]['label'] == "rumours":
+            data_y.append([1.0, 0.0])
+        else:
+            data_y.append([0.0, 1.0])
+    print("max_sent:", max_sent, ",  max_seq_len:", max(data_len))
+    tf.flags.DEFINE_integer("max_seq_len", max(data_len), "Max length of sequence (default: 300)")
+    tf.flags.DEFINE_integer("max_sent_len", max_sent, "Max length of sentence (default: 300)")
+    eval_flag = int(len(data_ID) / 4) * 3
+    print("{} data loaded".format(len(data)))
+
+
+def get_df_batch(start, new_data_len=[]):
+    data_x = np.zeros([FLAGS.batch_size, FLAGS.max_seq_len, FLAGS.max_sent_len, FLAGS.embedding_dim], dtype=np.float32)
+    m_data_y = np.zeros([FLAGS.batch_size, 2], dtype=np.int32)
+    m_data_len = np.zeros([FLAGS.batch_size], dtype=np.int32)
+    # miss_vec = 0
+    # hit_vec = 0
+    if len(new_data_len) > 0:
+        t_data_len = new_data_len
+    else:
+        t_data_len = data_len
+    mts = start * FLAGS.batch_size
+    if mts >= len(data_ID):
+        mts = mts % len(data_ID)
+
+    for i in range(FLAGS.batch_size):
+        m_data_y[i] = data_y[mts]
+        m_data_len[i] = t_data_len[mts]
+        for j in range(t_data_len[mts]):
+            t_words = data[data_ID[mts]]['text'][j]
+            # for k in range(len(t_words)):
+            #     m_word = t_words[k]
+            #     try:
+            #         # data_x[i][j][k] = c2vec.vectorize_words([m_word])[0]
+            #         data_x[i][j][k] = word2vec[m_word]
+            #     except KeyError:
+            #         print("word:", m_word)
+            #         miss_vec += 1
+            #     except IndexError:
+            #         print("i, j, k:", FLAGS.batch_size, '|',t_data_len[mts] ,'|', len(t_words))
+            #         print("word:", m_word, "(", i, j, k, ")")
+            #         raise
+            #     else:
+            #         hit_vec += 1
+            if len(t_words) != 0:
+                data_x[i][j][:len(t_words)] = c2vec.vectorize_words(t_words)
+        mts += 1
+        if mts >= len(data_ID): # read data looply
+            mts = mts % len(data_ID)
+
+    # print("hit_vec | miss_vec:", hit_vec, '|', miss_vec)
+    return data_x, m_data_len, m_data_y
+
+
+# seq_states is the date_x to get
+# max_id is the next corpus to take
+def get_rl_batch(ids, seq_states, stop_states, counter_id, start_id, total_data):
+    input_x = np.zeros([FLAGS.batch_size, FLAGS.max_sent_len, FLAGS.embedding_dim], dtype=np.float32)
+    input_y = np.zeros([FLAGS.batch_size, FLAGS.class_num], dtype=np.float32)
+    miss_vec = 0
+    total_data = len(data_len)
+
+    for i in range(FLAGS.batch_size):
+        # seq_states:records the id of a sentence in a sequence
+        # stop_states: records whether the sentence is judged by the program
+        if stop_states[i] == 1 or seq_states[i] >= data_len[ids[i]]: 
+            ids[i] = counter_id + start_id
+            seq_states[i] = 0
+            try:
+                t_words = data[ data_ID[ids[i]] ]['text'][seq_states[i]]
+            except:
+                print(ids[i], seq_states[i])
+            # for j in range(len(t_words)):
+            #     m_word = t_words[j]
+            #     try:
+            #         input_x[i][j] = word2vec[m_word]
+            #     except:
+            #         miss_vec = 1
+            if len(t_words) != 0:
+                input_x[i][:len(t_words)] = c2vec.vectorize_words(t_words)
+            input_y[i] = data_y[ids[i]]
+            counter_id += 1
+            counter_id = counter_id % total_data
+        else:
+            try:
+                t_words = data[ data_ID[ids[i]] ]['text'][seq_states[i]]
+            except:
+                print("ids and seq_states:", ids[i], seq_states[i])
+                t_words = []
+            # for j in range(len(t_words)):
+            #     m_word = t_words[j]
+            #     try:
+            #         input_x[i][j] = word2vec[m_word]
+            #     except:
+            #         miss_vec += 1
+            if len(t_words) != 0:
+                input_x[i][:len(t_words)] = c2vec.vectorize_words(t_words)
+            input_y[i] = data_y[ids[i]]
+        # point to the next sequence
+        seq_states[i] += 1
+
+    return input_x, input_y, ids, seq_states, counter_id
+
+
+# not to stop -0.1, so that to be early
+# DDQN y = r + Q(S, argmax(Q))
+def get_reward(isStop, ss, pys, ids, seq_ids):
+    global reward_counter
+    reward = np.zeros([len(isStop)], dtype=np.float32)
+    for i in range(len(isStop)):
+        if isStop[i] == 1:
+            if np.argmax(pys[ids[i]][seq_ids[i]-1]) == np.argmax(data_y[ids[i]]):
+                reward_counter += 1 # more number of correct prediction, more rewards
+                r = 1 + FLAGS.reward_rate * math.log(reward_counter)
+                reward[i] = r   
+            else:
+                reward[i] = -100
+        else:
+            reward[i] = -0.01 + 0.99 * max(ss[i])
+    return reward
+
+
+def get_new_len(sess, mm):
+    new_x_len = np.zeros([len(data_ID)], dtype=np.int32)
+
+    for i in range(len(data_ID)):
+        init_state = np.zeros([1, FLAGS.hidden_dim], dtype=np.float32)
+        e_state = sess.run(mm.df_state, feed_dict={mm.init_states: init_state})
+        for j in range(data_len[i]):
+            t_words = data[data_ID[i]]['text'][j]
+            e_x = np.zeros([1, FLAGS.max_sent_len, FLAGS.embedding_dim], dtype=np.float32)
+            # for k in range(len(t_words)):
+            #     m_word = t_words[k]
+            #     try:
+            #         e_x[0][k] = word2vec[m_word]
+            #     except:
+            #         miss_word = 1
+            if len(t_words) != 0:
+                e_x[0][:len(t_words)] = c2vec.vectorize_words(t_words)
+            batch_dic = {mm.rl_state: e_state, mm.rl_input: e_x, mm.dropout_keep_prob: 1.0}
+            e_isStop, mNewState = sess.run([mm.isStop, mm.rl_new_state], batch_dic)
+            e_state = mNewState
+
+            if e_isStop == 1:
+                new_x_len[i] = j+1
+                break
+        if new_x_len[i] == 0 or new_x_len[i] > data_len[i]:
+            new_x_len[i] = data_len[i]
+
+    # print(" Max Length: " + str(max(new_x_len)) +
+    #       " Min Length: " + str(min(new_x_len)) +
+    #       " Ave Length: " + str(np.mean(new_x_len))) + " (" + str(np.mean(data_len)) + ")"
+
+    return new_x_len
+
+
+def get_RL_Train_batch(D):
+    s_state = np.zeros([FLAGS.batch_size, FLAGS.hidden_dim], dtype=np.float32) 
+    s_x = np.zeros([FLAGS.batch_size, FLAGS.max_sent_len, FLAGS.embedding_dim], dtype=np.float32)
+    s_isStop = np.zeros([FLAGS.batch_size, FLAGS.action_num], dtype=np.float32)
+    s_rw = np.zeros([FLAGS.batch_size], dtype=np.float32)
+
+    m_batch = random.sample(D, FLAGS.batch_size)
+    for i in range(FLAGS.batch_size):
+        s_state[i] = m_batch[i][0]
+        s_x[i] = m_batch[i][1]
+        s_isStop[i][m_batch[i][2]] = 1
+        s_rw[i] = m_batch[i][3]
+
+    return s_state, s_x, s_isStop, s_rw
