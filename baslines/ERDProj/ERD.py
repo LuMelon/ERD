2e46c6c4ada91f270ff1b39297b1f86bf4d03b05
LuMelon <lumenglong2017@gmail.com>
2019-11-30 13:25:09 +0800

    使用layernorm的LSTM来代替先前的GRU

--- baslines/ERDProj/ERD.py
+++ baslines/ERDProj/ERD.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 import sys
+import random
 import torch
 import importlib
 from tensorboardX import SummaryWriter
@@ -9,10 +10,191 @@ import pickle
 import tqdm
 import os
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 from collections import deque
 sys.path.append(".")
 from dataUtilsV0 import *
 import json
+import pdb
+
+
+
+class LayerNormLSTMCell(nn.LSTMCell):
+    def __init__(self, input_size, hidden_size, dropout=0.0, bias=True, use_layer_norm=True):
+        super().__init__(input_size, hidden_size, bias)
+        self.use_layer_norm = use_layer_norm
+        if self.use_layer_norm:
+            self.ln_ih = nn.LayerNorm(4 * hidden_size)
+            self.ln_hh = nn.LayerNorm(4 * hidden_size)
+            self.ln_ho = nn.LayerNorm(hidden_size)
+        # DropConnect on the recurrent hidden to hidden weight
+        self.dropout = dropout
+
+    def forward(self, input, hidden=None):
+        self.check_forward_input(input)
+        if hidden is None:
+            hx = input.new_zeros(input.size(0), self.hidden_size, requires_grad=False)
+            cx = input.new_zeros(input.size(0), self.hidden_size, requires_grad=False)
+        else:
+            hx, cx = hidden
+        self.check_forward_hidden(input, hx, '[0]')
+        self.check_forward_hidden(input, cx, '[1]')
+
+        weight_hh = nn.functional.dropout(self.weight_hh, p=self.dropout, training=self.training)
+        if self.use_layer_norm:
+            gates = self.ln_ih(F.linear(input, self.weight_ih, self.bias_ih)) \
+                     + self.ln_hh(F.linear(hx, weight_hh, self.bias_hh))
+        else:
+            gates = F.linear(input, self.weight_ih, self.bias_ih) \
+                    + F.linear(hx, weight_hh, self.bias_hh)
+
+        i, f, c, o = gates.chunk(4, 1)
+        i_ = torch.sigmoid(i)
+        f_ = torch.sigmoid(f)
+        c_ = torch.tanh(c)
+        o_ = torch.sigmoid(o)
+        cy = (f_ * cx) + (i_ * c_)
+        if self.use_layer_norm:
+            hy = o_ * self.ln_ho(torch.tanh(cy))
+        else:
+            hy = o_ * torch.tanh(cy)
+        return hy, cy
+
+class LayerNormLSTM(nn.Module):
+    def __init__(self,
+                 input_size,
+                 hidden_size,
+                 num_layers=1,
+                 dropout=0.0,
+                 weight_dropout=0.0,
+                 bias=True,
+                 bidirectional=False,
+                 use_layer_norm=True):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        # using variational dropout
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+
+        num_directions = 2 if bidirectional else 1
+        self.hidden0 = nn.ModuleList([
+            LayerNormLSTMCell(input_size=(input_size if layer == 0 else hidden_size * num_directions),
+                              hidden_size=hidden_size, dropout=weight_dropout, bias=bias, use_layer_norm=use_layer_norm)
+            for layer in range(num_layers)
+        ])
+
+        if self.bidirectional:
+            self.hidden1 = nn.ModuleList([
+                LayerNormLSTMCell(input_size=(input_size if layer == 0 else hidden_size * num_directions),
+                                  hidden_size=hidden_size, dropout=weight_dropout, bias=bias, use_layer_norm=use_layer_norm)
+                for layer in range(num_layers)
+            ])
+
+    def copy_parameters(self, rnn_old):
+        for param in rnn_old.named_parameters():
+            name_ = param[0].split("_")
+            layer = int(name_[2].replace("l", ""))
+            sub_name = "_".join(name_[:2])
+            if len(name_) > 3:
+                self.hidden1[layer].register_parameter(sub_name, param[1])
+            else:
+                self.hidden0[layer].register_parameter(sub_name, param[1])
+
+    def forward(self, input, hidden=None, seq_lens=None):
+        seq_len, batch_size, _ = input.size()
+        num_directions = 2 if self.bidirectional else 1
+        if hidden is None:
+            hx = input.new_zeros(self.num_layers * num_directions, batch_size, self.hidden_size, requires_grad=False)
+            cx = input.new_zeros(self.num_layers * num_directions, batch_size, self.hidden_size, requires_grad=False)
+        else:
+            hx, cx = hidden
+
+        ht = []
+        for i in range(seq_len):
+            ht.append([None] * (self.num_layers * num_directions))
+        ct = []
+        for i in range(seq_len):
+            ct.append([None] * (self.num_layers * num_directions))
+
+        seq_len_mask = input.new_ones(batch_size, seq_len, self.hidden_size, requires_grad=False)
+        if seq_lens != None:
+            for i, l in enumerate(seq_lens):
+                seq_len_mask[i, l:, :] = 0
+        seq_len_mask = seq_len_mask.transpose(0, 1)
+
+        if self.bidirectional:
+            # if use cuda, change 'torch.LongTensor' to 'torch.cuda.LongTensor'
+            indices_ = (torch.LongTensor(seq_lens) - 1).unsqueeze(1).unsqueeze(0).unsqueeze(0).repeat(
+                [1, 1, 1, self.hidden_size])
+            # if use cuda, change 'torch.LongTensor' to 'torch.cuda.LongTensor'
+            indices_reverse = torch.LongTensor([0] * batch_size).unsqueeze(1).unsqueeze(0).unsqueeze(0).repeat(
+                [1, 1, 1, self.hidden_size])
+            indices = torch.cat((indices_, indices_reverse), dim=1)
+            hy = []
+            cy = []
+            xs = input
+            # Variational Dropout
+            if not self.training or self.dropout == 0:
+                dropout_mask = input.new_ones(self.num_layers, 2, batch_size, self.hidden_size)
+            else:
+                dropout_mask = input.new(self.num_layers, 2, batch_size, self.hidden_size).bernoulli_(1 - self.dropout)
+                dropout_mask = Variable(dropout_mask, requires_grad=False) / (1 - self.dropout)
+
+            for l, (layer0, layer1) in enumerate(zip(self.hidden0, self.hidden1)):
+                l0, l1 = 2 * l, 2 * l + 1
+                h0, c0, h1, c1 = hx[l0], cx[l0], hx[l1], cx[l1]
+                for t, (x0, x1) in enumerate(zip(xs, reversed(xs))):
+                    ht_, ct_ = layer0(x0, (h0, c0))
+                    ht[t][l0] = ht_ * seq_len_mask[t]
+                    ct[t][l0] = ct_ * seq_len_mask[t]
+                    h0, c0 = ht[t][l0], ct[t][l0]
+                    t = seq_len - 1 - t
+                    ht_, ct_ = layer1(x1, (h1, c1))
+                    ht[t][l1] = ht_ * seq_len_mask[t]
+                    ct[t][l1] = ct_ * seq_len_mask[t]
+                    h1, c1 = ht[t][l1], ct[t][l1]
+
+                xs = [torch.cat((h[l0]*dropout_mask[l][0], h[l1]*dropout_mask[l][1]), dim=1) for h in ht]
+                ht_temp = torch.stack([torch.stack([h[l0], h[l1]]) for h in ht])
+                ct_temp = torch.stack([torch.stack([c[l0], c[l1]]) for c in ct])
+                if len(hy) == 0:
+                    hy = torch.stack(list(ht_temp.gather(dim=0, index=indices).squeeze(0)))
+                else:
+                    hy = torch.cat((hy, torch.stack(list(ht_temp.gather(dim=0, index=indices).squeeze(0)))), dim=0)
+                if len(cy) == 0:
+                    cy = torch.stack(list(ct_temp.gather(dim=0, index=indices).squeeze(0)))
+                else:
+                    cy = torch.cat((cy, torch.stack(list(ct_temp.gather(dim=0, index=indices).squeeze(0)))), dim=0)
+            y  = torch.stack(xs)
+        else:
+            # if use cuda, change 'torch.LongTensor' to 'torch.cuda.LongTensor'
+            indices = (torch.cuda.LongTensor(seq_lens) - 1).unsqueeze(1).unsqueeze(0).unsqueeze(0).repeat(
+                [1, self.num_layers, 1, self.hidden_size])
+            h, c = hx, cx
+            # Variational Dropout
+            if not self.training or self.dropout == 0:
+                dropout_mask = input.new_ones(self.num_layers, batch_size, self.hidden_size)
+            else:
+                dropout_mask = input.new(self.num_layers, batch_size, self.hidden_size).bernoulli_(1 - self.dropout)
+                dropout_mask = Variable(dropout_mask, requires_grad=False) / (1 - self.dropout)
+
+            for t, x in enumerate(input):
+                for l, layer in enumerate(self.hidden0):
+                    ht_, ct_ = layer(x, (h[l], c[l]))
+                    ht[t][l] = ht_ * seq_len_mask[t]
+                    ct[t][l] = ct_ * seq_len_mask[t]
+                    x = ht[t][l] * dropout_mask[l]
+                ht[t] = torch.stack(ht[t])
+                ct[t] = torch.stack(ct[t])
+                h, c = ht[t], ct[t]
+            y = torch.stack([h[-1]*dropout_mask[-1] for h in ht])
+            hy = torch.stack(list(torch.stack(ht).gather(dim=0, index=indices).squeeze(0)))
+            cy = torch.stack(list(torch.stack(ct).gather(dim=0, index=indices).squeeze(0)))
+
+        return y, (hy, cy)
 
 
 # ### 模型训练与测试
@@ -54,6 +236,28 @@ class RDM_Model(nn.Module):
             print("Error:", pool_feature.shape, init_states.shape)
             raise
         return df_outputs
+
+class RDM_Model_V1(nn.Module):
+    def __init__(self, word_embedding_dim, sent_embedding_dim, hidden_dim, dropout_prob):
+        super(RDM_Model_V1, self).__init__()
+        self.embedding_dim = sent_embedding_dim
+        self.hidden_dim = hidden_dim
+        self.gru_model = LayerNormLSTM(word_embedding_dim, 
+                                self.hidden_dim, 
+                                dropout=dropout_prob
+                            )
+
+    def forward(self, input_x, seq_lens): 
+        """
+        input_x: [batchsize, max_seq_len, sentence_embedding_dim] 
+        x_len: [batchsize]
+        init_states: [batchsize, hidden_dim]
+        """
+        batchsize, max_seq_len, emb_dim = input_x.shape
+        h0 = torch.zeros([1, batchsize, self.hidden_dim], dtype=torch.float32).cuda()
+        c0 = torch.zeros([1, batchsize, self.hidden_dim], dtype=torch.float32).cuda()
+        df_outputs, (df_last_state, df_last_cell) = self.gru_model(input_x.transpose(0, 1), (h0, c0), seq_lens)
+        return df_outputs.transpose(0, 1), df_last_state.transpose(0, 1), df_last_cell.transpose(0, 1)
 
 class CM_Model_V1(nn.Module):
     def __init__(self, hidden_dim, action_num):
@@ -156,7 +360,8 @@ def TrainRDMModel(rdm_model, sent_pooler, rdm_classifier,
         try:
             x, x_len, y = get_df_batch(step*batch_size, batch_size)
             seq = sent_pooler(x)
-            rdm_hiddens = rdm_model(seq)
+            # rdm_hiddens = rdm_model(seq)
+            rdm_hiddens, rdm_out, rdm_cell = rdm_model(seq, x_len.tolist())
             batchsize, _, _ = rdm_hiddens.shape
             rdm_outs = torch.cat(
                 [ rdm_hiddens[i][x_len[i]-1].unsqueeze(0) for i in range(batchsize)] 
@@ -198,12 +403,12 @@ def TrainRDMModel(rdm_model, sent_pooler, rdm_classifier,
             print('%3d | %d , train_loss/accuracy = %6.8f/%6.7f'             % (step, t_steps, 
                 sum_loss, sum_acc,
                 ))
-            if step%100 == 99:
-                valid_acc = accuracy_on_valid_data(rdm_model, sent_pooler, rdm_classifier)
-                if valid_acc > best_valid_acc:
-                    print("valid_acc:", valid_acc)
-                    writer.add_scalar('Valid Accuracy', valid_acc, step)
-                    best_valid_acc = valid_acc
+            if step%500 == 499:
+                # valid_acc = accuracy_on_valid_data(rdm_model, sent_pooler, rdm_classifier)
+                # if valid_acc > best_valid_acc:
+                #     print("valid_acc:", valid_acc)
+                #     writer.add_scalar('Valid Accuracy', valid_acc, step)
+                #     best_valid_acc = valid_acc
                     rdm_save_as = '%s/ERD_best.pkl'% (log_dir)
                     torch.save(
                         {
@@ -306,19 +511,129 @@ def TrainCMModel(sent_pooler, rdm_model, rdm_classifier, cm_model, stage, t_rw, 
                 D.popleft()
         counter += 1
 
+
+def TrainCMModel_V2(sent_pooler, rdm_model, rdm_classifier, cm_model, stage, t_rw, t_steps, log_dir, logger, FLAGS, cuda=True):
+    batch_size = 20
+    t_acc = 0.9
+    gamma = 1.0
+    lambda1 = -1.0
+    lambda2 = 0.0 #regularizer
+    sum_loss = 0.0
+    sum_acc = 0.0
+    t_acc = 0.9
+    ret_acc = 0.0
+    init_states = torch.zeros([1, batch_size, rdm_model.hidden_dim], dtype=torch.float32).cuda()
+    weight = torch.tensor([2.0, 1.0], dtype=torch.float32).cuda()
+    loss_fn = nn.CrossEntropyLoss(weight=weight)
+    optim = torch.optim.Adagrad([
+                                {'params': sent_pooler.parameters(), 'lr': 5e-5},
+                                {'params': rdm_model.parameters(), 'lr': 5e-5},
+                                {'params': rdm_classifier.parameters(), 'lr': 5e-5},
+                                {'params': cm_model.parameters(), 'lr':2e-3}
+                             ]
+    )
+    
+    writer = SummaryWriter(log_dir, filename_suffix="_ERD_CM_stage_%3d"%stage)
+    best_valid_acc = 0.0
+
+    rw_arr = np.zeros(10)
+    len_arr = np.zeros(10)
+    for step in range(t_steps):
+            x, x_len, y = get_df_batch(step*batch_size, batch_size)        
+        # for rep in range(100):
+            optim.zero_grad()
+            seq = sent_pooler(x)
+            rdm_hiddens = rdm_model(seq)
+            batchsize, _, _ = rdm_hiddens.shape
+            rdm_outs = torch.cat(
+                [ rdm_hiddens[i][x_len[i]-1].unsqueeze(0) for i in range(batchsize)] 
+                # a list of tensor, where the ndim of tensor is 1 and the shape of tensor is [hidden_size]
+            )
+            rdm_scores = rdm_classifier(
+                rdm_outs
+            )
+            rdm_preds = rdm_scores.argmax(axis=1)
+            y_label = torch.tensor(y).argmax(axis=1).cuda() if cuda else torch.tensor(y).argmax(axis=1)
+            acc, _, _, _, _, _, _ = Count_Accs(y_label, rdm_preds)
+            loss = loss_fn(rdm_scores, y_label)
+            
+            batchsize, max_seq_len, _ = rdm_hiddens.shape
+            stopScore, isStop = cm_model(rdm_hiddens.reshape(-1, 256))
+            isStop = isStop.reshape([batchsize, max_seq_len, -1])
+            stopScore = stopScore.reshape([batchsize, max_seq_len, -1]).softmax(axis=-1)         
+            E_rw = torch.zeros(len(x_len)).cuda()
+            prob = torch.ones(len(x_len)).cuda()
+            sum_len = 0.0
+            for j in range(len(x_len)):
+                sum_rw = 0.0
+                # start = 0
+                start = random.randint(0, x_len[j]-1)
+                for t in range(start, x_len[j]):
+                    rnd = random.random()
+                    # pdb.set_trace()
+                    if rnd > stopScore[j][t][1]:
+                        rw = -2.0/x_len[j]
+                        sum_rw += pow(gamma, t)*rw
+                        prob[j] *= stopScore[j][t][0]            
+                    else:
+                        if y[j][1] == 1:
+                            rw = 1.0
+                        else:
+                            rw = -1.0
+                        sum_rw += pow(gamma, t)*rw
+                        prob[j] *= stopScore[j][t][1]
+                        rnd2 = random.random()
+                        if  rnd2 > 0.6:
+                            prob[j] *= 0.4
+                            continue
+                        else:
+                            prob[j] *= 0.6
+                            break
+                
+                # sum_len += (t-start+1)*1.0/x_len[j]
+                sum_len += (t-start+1)
+                E_rw[j] = prob[j]*sum_rw
+            pdb.set_trace()
+            rw_arr[int(step%10)] = E_rw.mean()
+            len_arr[int(step%10)] = sum_len*1.0/batch_size
+
+            loss_back = lambda1*E_rw.mean() + lambda2*loss
+            loss_back.backward()
+            optim.step()      
+            if step%10 == 0:  
+                # print('%3d | %d , train_loss/Expected Reward = %6.8f/%6.7f,  RDM_Loss/RDM Accuracy = %6.8f/%6.7f, mean_len = %2.3f'             % (step, t_steps, 
+                #         loss_back, E_rw.mean(), loss, acc, sum_len*1.0/batch_size
+                #         ))
+                print('%3d | %d , train_loss/Expected Reward = %6.8f/%6.7f,  RDM_Loss/RDM Accuracy = %6.8f/%6.7f, mean_len = %2.3f'             % (step, t_steps, 
+                        loss_back, rw_arr.mean(), loss, acc, len_arr.mean()
+                        ))
+
+            writer.add_scalar('RDM Loss', loss, step)
+            writer.add_scalar('RDM Accuracy', acc, step)
+            writer.add_scalar('Train Loss', loss_back, step)
+            writer.add_scalar('Expected Reward', E_rw.mean(), step)
+
+            torch.cuda.empty_cache()
+        
+        # print("weight grad:", cm_model.Classifier.weight.grad)
+        # print("bias grad:", cm_model.Classifier.bias.grad)
+    print(get_curtime() + " Train df Model End.")
+    return ret_acc    
+
+
 # In[13]:
 
 
 load_data_fast()
 
-rdm_model = RDM_Model(300, 300, 256, 0.2).cuda()
+rdm_model = RDM_Model_V1(300, 300, 256, 0.2).cuda()
 sent_pooler = pooling_layer(300, 300).cuda()
 rdm_classifier = nn.Linear(256, 2).cuda()
 cm_model = CM_Model_V1(256, 2).cuda()
 
-log_dir = os.path.join(sys.path[0], "ERD/")
+log_dir = os.path.join(sys.path[0], "ERDV1/")
 
-with open("config.json", "r") as cr:
+with open("../../config.json", "r") as cr:
     dic = json.load(cr)
 
 class adict(dict):
@@ -349,9 +664,9 @@ else:
 for i in range(20):
     erd_save_as = '%s/erdModel_epoch%03d.pkl'% (log_dir , i)
     if i==0:
-        TrainCMModel(sent_pooler, rdm_model, rdm_classifier, cm_model, 0, 0.5, 20000, log_dir, None, FLAGS, cuda=True)
+        TrainCMModel_V2(sent_pooler, rdm_model, rdm_classifier, cm_model, 0, 0.5, 20000, log_dir, None, FLAGS, cuda=True)
     else:
-        TrainCMModel(sent_pooler, rdm_model, rdm_classifier, cm_model, 0, 0.5, 2000, log_dir, None, FLAGS, cuda=True)
+        TrainCMModel_V2(sent_pooler, rdm_model, rdm_classifier, cm_model, 0, 0.5, 2000, log_dir, None, FLAGS, cuda=True)
     torch.save(
         {
             "sent_pooler":sent_pooler.state_dict(),
@@ -362,13 +677,13 @@ for i in range(20):
         erd_save_as
     )
     print("iter:", i, ", train cm model completed!")
-    new_len, valid_new_len = get_new_len(sent_pooler, rdm_model, cm_model, FLAGS, cuda=True)
-    print("after new len:")
-    print("new_data_len:", new_len)
-    print("valid_new_len:", valid_new_len)
-    TrainRDMModel(rdm_model, sent_pooler, rdm_classifier, 
-                    t_steps=1000, stage=0, new_data_len=[], valid_new_len=[], logger=None, 
-                        log_dir=log_dir, cuda=True)
+    # new_len, valid_new_len = get_new_len(sent_pooler, rdm_model, cm_model, FLAGS, cuda=True)
+    # print("after new len:")
+    # print("new_data_len:", new_len)
+    # print("valid_new_len:", valid_new_len)
+    # TrainRDMModel(rdm_model, sent_pooler, rdm_classifier, 
+    #                 t_steps=1000, stage=0, new_data_len=[], valid_new_len=[], logger=None, 
+    #                     log_dir=log_dir, cuda=True)
 
 
 
